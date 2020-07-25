015a46a97f0dfefdf82784979e8e5abf55edd914
Alessandra Henkes Nepomoceno <alessandra.henkes@cwi.com.br>
2020-07-24 23:25:45 -0300

    Adicionando conteudo inicial

--- app/src/main/java/com/example/rickverse/characters/CharacterActivity.kt
+++ app/src/main/java/com/example/rickverse/characters/CharacterActivity.kt
@@ -1,12 +1,17 @@
 package com.example.rickverse.characters
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import com.example.rickverse.R
 
 class CharacterActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_character)
+        setUI()
     }
+
+    private fun setUI() {}
+
 }
--- app/src/main/java/com/example/rickverse/splash/SplashActivity.kt
+++ app/src/main/java/com/example/rickverse/splash/SplashActivity.kt
@@ -1,12 +1,18 @@
 package com.example.rickverse.splash
 
 import android.os.Bundle
+import android.os.Handler
 import androidx.appcompat.app.AppCompatActivity
 import com.example.rickverse.R
 
+private const val SPLASH_DELAY = 2000L
+
 class SplashActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
+        Handler().postDelayed({}, SPLASH_DELAY)
     }
+
 }
